ebf7396b255501e41d6cca34e628a21999119302
Kavita Jain <kv1jn7@gmail.com>
2025-05-16 22:22:08 +0000

    added repo

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,9 @@
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.explorecalijpa.model.TourPackage;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
+}
---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,9 @@
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.explorecalijpa.model.Tour;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
